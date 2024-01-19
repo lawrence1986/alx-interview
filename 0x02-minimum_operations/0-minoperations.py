c295922ddc29baa9c620d3b01e45bef74e426bf0
lawrence1986 <madusquare@gmail.com>
2024-01-19 07:38:50 +0000

    changed script

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
+"""Module which contains minoperations function"""
+
+
 def minOperations(n):
-    if n == 1:
-        return 0  # No operations needed for one 'H'
-
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(2, i + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-    
-    return dp[n] if dp[n] != float('inf') else 0
-
-# Example usage:
-n = 8
-result = minOperations(n)
-print(f"The fewest number of operations needed to achieve {n} 'H' characters is: {result}")
+    '''return'''
+    if n <= 1:
+        return 0
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
